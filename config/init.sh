9bc2b37a178e7d3c993ef4d2aa43778e0182cb7c
Andrew Joslin <andrewtjoslin@gmail.com>
2016-03-12 14:35:29 -0800

    Update init.sh to, yunno, work

--- config/init.sh
+++ config/init.sh
@@ -1,29 +1,63 @@
 #!/bin/sh
 
-# echo "-- BREW INSTALL"
-# brew update
-# brew tap thoughtbot/formulae
-# brew install git node tmux vim the_silver_searcher zsh rcm lua luarocks
+echo "Sign in as sudo, we'll need it..."
 
-# echo "-- RCUP"
-# cd ~/dot
-# cp rcrc ~/.rcrc
-# rcup 
+# Ask for the administrator password upfront
+sudo -v
 
-# echo "-- ZSH DEFAULT"
-# sudo echo "$(which zsh)" >> /etc/shells
-# chsh -s zsh
-# zsh
+set -x
 
-echo "-- NPM INSTALLS"
-npm install -g gulp karma httpster
+echo "-- brew install"
+brew update
+brew tap thoughtbot/formulae
+brew install git node tmux vim the_silver_searcher zsh rcm lua luarocks caius/jo/jo
+brew install go --cross-compile-common
 
-echo "-- MJOLNIR"
+mkdir -p $HOME/gocode
+
+echo "-- configure zsh"
+sudo bash -c "echo $(which zsh) >> /etc/shells"
+chsh -s $(which zsh)
+
+echo "-- Clone dotfiles"
+cd $HOME
+git clone git@github.com:ajoslin/dot --recursive
+rcup -d ~/dot
+
+echo "-- Installing global packages"
+npm install -g babel-cli \
+  bower \
+  browserify-size \
+  clocker \
+  cordova \
+  degree \
+  generator-bd \
+  github-markdown-preview \
+  httpster \
+  invoicer \
+  ionic \
+  ios-deploy \
+  ios-sim \
+  linklocal \
+  n \
+  ngrok \
+  node-sass \
+  nodemon \
+  pnpm \
+  react-native-cli \
+  standard \
+  standard-format \
+  yo \
+  tap-spec \
+  tape
+
+echo "-- Installing Mjolnir..."
 luarocks install mjolnir.hotkey
 luarocks install mjolnir.application
+
 cd ~/Downloads
-curl https://github.com/sdegutis/mjolnir/releases/download/0.4.3/Mjolnir-0.4.3.zip | unzip
-unzip Mjolnir-0.4.3.zip
-mv Mjolnir /Applications/
+curl -L -o mjolnir.zip https://github.com/sdegutis/mjolnir/releases/download/0.4.3/Mjolnir-0.4.3.zip
+unzip mjolnir.zip
+sudo mv Mjolnir.app /Applications/
 
 ./osx.sh
